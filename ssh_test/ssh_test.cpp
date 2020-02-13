85655ccb8669aa8d2c9fabedda84eb7d7ef15f0f
Anton Albertovich <38799794+AntonAlbertovich@users.noreply.github.com>
2020-02-13 17:12:49 -0600

    Added comments

--- ssh_test/ssh_test.cpp
+++ ssh_test/ssh_test.cpp
@@ -1,10 +1,15 @@
 #include <iostream>
 using namespace std;
 
+//This program tests out the ssh system call. It should be compiled and ran from the Ubuntu terminal on your Windos machine
+
+// compile with $ g++ ssh_test.cpp -o runthisc
+// run with $ ./runthisc
+
 int main() {
     system("pwd");
-    system("sshpass -p 'somepassword' ssh -Y username@192.168.1.xxx");
-    //system("pwd");
+    system("sshpass -p 'somepassword' ssh -Y username@192.168.1.xxx"); //use this line if you have installed sshpass
+    //system("ssh -Y username@192.168.1.xxx"); //use this line if you have disabled the password for the server
 
     return 0;
 }
