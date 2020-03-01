e6160f52858dfabd12d578937864e2d03bc27c89
Sarah Dominy <40004337+SarahDominy@users.noreply.github.com>
2020-02-29 22:25:19 -0600

    Create serverside.cpp

---
+++ GUI/serverside.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+//This program will run other important terminal commands including the next part of the GUI. It should be compiled and ran from the Ubuntu terminal on your Windos machine
+
+// compile with $ g++ serverside.cpp -o runningcinthenineties
+// run with $ ./runningcinthenineties
+
+int main() {
+        system("python3.7 Jackalope_server_gui.py"); //use this line if you have installed sshpass
+
+
+        return 0;
+        }
