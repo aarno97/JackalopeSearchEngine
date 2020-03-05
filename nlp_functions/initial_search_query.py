1c54bdf617df14a949bf05881548f96d830b2e6c
Anton Albertovich <38799794+AntonAlbertovich@users.noreply.github.com>
2020-03-05 12:16:38 -0600

    Moved decoy into their own functions

--- nlp_functions/initial_search_query.py
+++ nlp_functions/initial_search_query.py
@@ -5,8 +5,30 @@
 
 # This script houses the functions for processing an initial search query.
 
+def get_decoys(target):
+	decoy1_search = []
+	import wikipedia
+	while len(decoy1_search) < target:
+		try:
+			decoy1 = wikipedia.page(wikipedia.random())
+			decoy1_query = wikipedia.search(decoy1)
+			
+			for i in range(len(decoy1_query)):
+				if len(decoy1_search) >= target:
+				
+					break
+				else:
+					try:	
+						member0 = wikipedia.page(decoy1_query[i])
+						decoy1_search += member0.references
+						
+					except:	
+						continue
 
+		except:
+			continue
 
+	return decoy1_search
 
 def tokenize_search_query(search_query):
 	import wikipedia
@@ -27,46 +49,10 @@ def tokenize_search_query(search_query):
 		except:
 			continue
 	
-	decoy1_search = []
-	decoy2_search = []
+	decoy1_search = get_decoys(len(basic_search))
+	decoy2_search = get_decoys(len(basic_search))
 
-	while len(decoy1_search) < len(basic_search):
-		try:
-			decoy1 = wikipedia.page(wikipedia.random())
-			decoy1_query = wikipedia.search(decoy1)
 			
-			for i in range(len(decoy1_query)):
-				if len(decoy1_search) >= len(basic_search):
-					break
-				else:
-					try:	
-						member0 = wikipedia.page(decoy1_query[i])
-						decoy1_search += member0.references
-						
-					except:	
-						continue
-
-		except:
-			continue
-			
-	while len(decoy2_search) < len(basic_search):
-		try:
-			decoy2 = wikipedia.page(wikipedia.random())
-			decoy2_query = wikipedia.search(decoy2)
-			
-			for i in range(len(decoy2_query)):
-				if len(decoy2_search) >= len(basic_search):
-					break
-				else:
-					try:	
-						member0 = wikipedia.page(decoy2_query[i])
-						decoy2_search += member0.references 	
-
-					except:
-						continue
-
-		except:
-			continue
 
 	wiki_pages = []
 
@@ -76,5 +62,7 @@ def tokenize_search_query(search_query):
 	print(len(decoy1_search))
 	print(len(decoy2_search))
 		
+
+
 
 tokenize_search_query("Paul Newman")
