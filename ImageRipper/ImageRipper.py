9cec6c2c74451559a13005c15a1f39fd70196cba
Aaron Arnold <pape.aaron97@icloud.com>
2020-03-11 19:55:41 -0500

    Created ImageRipper, currently works on most websites

---
+++ ImageRipper/ImageRipper.py
@@ -0,0 +1,46 @@
+"""
+Sources:
+https://www.thepythoncode.com/article/download-web-page-images-python
+
+How to run this program in terminal:
+
+"""
+
+# import libraries
+import re
+from urllib.parse import urlparse, urljoin
+import requests
+from bs4 import BeautifulSoup
+
+
+# define images, a URL sent to this will save all images
+def images(url):
+    # use requests to get the text of the HTML
+    page = requests.get(url).text
+
+    # pass page to BeautifulSoup to parse
+    soup = BeautifulSoup(page, 'html.parser')
+
+    # refactor parsed page to return urls of images
+    img_urls = soup.find_all('img')
+
+    # put all image links into a list
+    urls = [img['src'] for img in img_urls]
+
+    # using set() to remove duplicates
+    nodupes = list(set(urls))
+
+    # make urls absolute in for in the event we don't get the full url in the beginning
+    absolute = []
+    for image in nodupes:
+        absolute.append(urljoin(url, image))
+
+    # print all links
+    for reference in absolute:
+        print(reference)
+    return
+
+
+url = "https://unsplash.com"
+print(url)
+images(url)
