9833ff396f87b25f3f4b8744bb0606f247fc5f1e
Aaron Arnold <pape.aaron97@icloud.com>
2020-04-28 18:12:07 -0500

    Working on displaying image in TK

---
+++ ImageRipper/DisplayImage.py
@@ -0,0 +1,20 @@
+"""
+Sources:
+https://www.daniweb.com/programming/software-development/code/440946/display-an-image-from-a-url-tkinter-python
+
+"""
+
+import tkinter as tk
+from PIL import ImageTk, Image
+import os
+import requests
+from io import BytesIO
+
+root = tk.Tk()
+img_url = "https://media.giphy.com/media/jXOxSiAx5UVnq/giphy.gif"
+response = requests.get(img_url)
+img_data = response.content
+img = ImageTk.PhotoImage(Image.open(BytesIO(img_data)))
+panel = tk.Label(root, image=img)
+panel.pack(side="bottom", fill="both", expand="yes")
+root.mainloop()
